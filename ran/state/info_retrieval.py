c1ed5e083082eb8e795da9008bc7b79b67fad46f
Ameer Arsala <ameer.arsala03@gmail.com>
2024-07-08 22:23:36 -0700

    added to info_retrieval.py

--- ran/state/info_retrieval.py
+++ ran/state/info_retrieval.py
@@ -1,10 +1,16 @@
+import os
+import shutil
+
 from typing import List, Dict, Set, Union
 from pydantic import BaseModel, Field
 
 from state.ranstate import PaperInstallation, PaperImplID
 from state.ranstate import RanPaperInstallation, PythonPackageDependency
 
-from constants import RAN_REGISTRY_GIT_HTTPS_URL
+from constants import LIB_ROOT, RAN_REGISTRY_GIT_HTTPS_URL, RAN_DEFAULT_AUTHOR_NAME
+
+from git import Repo
+import yaml
 
 
 # Example registry.yaml
@@ -16,7 +22,7 @@ attention_is_all_you_need:
               repo_url: https://github.com/...
               dependencies:
                   - numpy==1.23.1
-                  - pandas
+                  - pandas==...
     ameerarsala:
         versions:
             ...
@@ -27,9 +33,93 @@ attention_is_all_you_need:
 """
 
 
-# TODO:
+class PaperImplementationVersion(BaseModel):
+    tag: str
+    repo_url: str
+    dependencies: List[str]
+
+    def as_python_package_dependencies(
+        self, isolated: bool
+    ) -> List[PythonPackageDependency]:
+        pypackage_deps: List[PythonPackageDependency] = []
+
+        for dependency in self.dependencies:
+            equals_idx: int = dependency.index("==")
+
+            package_name: str = dependency[0:equals_idx]
+            version: str = dependency[equals_idx + 2 :]
+
+            pypackage_deps.append(
+                PythonPackageDependency(
+                    package_name=package_name, version=version, isolated=isolated
+                )
+            )
+
+        return pypackage_deps
+
+
+def load_registry(update_registry: bool = True) -> Dict:
+    if update_registry:
+        print("Updating Registry...")
+        try:
+            shutil.rmtree(f"{LIB_ROOT}/ran-registry")
+        except OSError:
+            pass
+
+        # Clone
+        Repo.clone_from(
+            RAN_REGISTRY_GIT_HTTPS_URL, LIB_ROOT
+        )  # clones into LIB_ROOT/ran-registry/*
+
+    with open(f"{LIB_ROOT}/ran-registry/registry.yaml") as registry_file:
+        registry: Dict = yaml.safe_load(registry_file)
+
+    return registry
+
+
+def _find_matching_version_idx(versions: List[Dict], nametag: str) -> int:
+    for version, idx in enumerate(versions):
+        if version["tag"] == nametag:
+            return idx
+
+    return -1
+
+
+def fetch_paper_implementation_version(
+    paper_impl_id: PaperImplID, registry: Dict
+) -> PaperImplementationVersion:
+    versions: List[Dict] = registry[paper_impl_id.paper_id][paper_impl_id.author][
+        "versions"
+    ]
+
+    # Get correct version index based on
+    idx: int = 0
+    if paper_impl_id.tag == "latest":
+        idx = -1
+    elif paper_impl_id.tag == "earliest":
+        idx = 0
+    else:
+        # This implies the tag is exact
+        # Find idx by nametag
+        idx = _find_matching_version_idx(versions, paper_impl_id.tag)
+
+    version: PaperImplementationVersion = PaperImplementationVersion(**versions[idx])
+
+    return version
+
+
+def fetch_repo_url(paper_impl_id: PaperImplID, update_registry: bool = False) -> str:
+    registry: Dict = load_registry(update_registry)
+
+    version: PaperImplementationVersion = fetch_paper_implementation_version(
+        paper_impl_id, registry
+    )
+
+    return version.repo_url
+
+
 def fetch_dependencies(
-    paper_installations: List[PaperInstallation],
+    paper_installations: List[PaperInstallation], update_registry: bool = True
 ) -> List[RanPaperInstallation]:
     """
     Fetch from DB to get the required python package names and versions, then return the as List[RanPaperInstallation]
@@ -38,5 +128,31 @@ def fetch_dependencies(
     """
     # 0.) Git clone latest, which consists of removing the existing one then git cloning
     # 1.) Read locally and process tags like 'latest' into their actual values (their actual verbose values for maximum reproducibility)
-    # 2.) Fetch the dependencies based on that and wrap in a nice RanPaperInstallation
-    pass
+    registry: Dict = load_registry(update_registry=update_registry)
+
+    # Fetch dependencies
+    ran_paper_installations: List[RanPaperInstallation] = []
+    for paper_installation in paper_installations:
+        paper_impl_id: PaperImplID = paper_installation.paper_impl_id
+        isolate_packages: bool = paper_installation.isolate
+
+        version: PaperImplementationVersion = fetch_paper_implementation_version(
+            paper_impl_id, registry
+        )
+
+        # Process tags like 'latest' and 'earliest' into their respective tags
+        # (Use EXACT tag, no aliases)
+        paper_impl_id.tag = version.tag
+
+        # Fetch dependencies
+        package_dependencies: List[PythonPackageDependency] = (
+            version.as_python_package_dependencies(isolate_packages)
+        )
+
+        ran_paper_installations.append(
+            RanPaperInstallation(
+                paper_impl_id=paper_impl_id, package_dependencies=package_dependencies
+            )
+        )
+
+    return ran_paper_installations
