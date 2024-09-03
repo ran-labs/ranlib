ffb09d84faea417ebb6e659f93fb9df45321f256
Ameer Arsala <ameer.arsala03@gmail.com>
2024-09-02 19:24:37 -0700

    added windows support

--- ranlib/cli/subcmds/auth.py
+++ ranlib/cli/subcmds/auth.py
@@ -2,10 +2,14 @@ from typing import List, Dict, Union, Optional
 
 import typer
 
+# Helpers
+from ranlib.cli.helpers import pre, manifest_project_root, check_pixi_installation
+
 
 app = typer.Typer()
 
 
 @app.command()
 def login():
+    """Log into RAN. Useful for publishing. Or if you want to access private stuff"""
     pass
