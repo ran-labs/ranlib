816f910588bbfe52644aa32c0dce706f5151d3ec
Ameer Arsala <ameer.arsala03@gmail.com>
2024-10-01 20:00:53 -0700

    fixed the license ci/cd bug

--- scripts/helpers/recipe-preprocessor.py
+++ scripts/helpers/recipe-preprocessor.py
@@ -10,16 +10,16 @@ def modify_recipe(recipe_path: str):
     # Modify it
 
     # For whatever reason, rattler-build does not copy in the LICENSE so we have to take it manually
-    # Luckily, since the repo is public, we have a free automation by using the LICENSE in our own repo!
-    #LICENSE_URL: str = "https://raw.githubusercontent.com/anemo-ai/ranlibx/refs/heads/main/LICENSE"
-    #curl_cmd: str = f"curl {LICENSE_URL} -o ./LICENSE"
+    # TODO: once the repo is public, use our own repo's LICENSE file for a free automation
+    # For now, just use the one in this link
+    LICENSE_URL: str = "https://raw.githubusercontent.com/AmeerArsala/apache-license/refs/heads/main/LICENSE"
+    curl_cmd: str = f"curl {LICENSE_URL} -o ./LICENSE"
 
     # Python install command
     py_install_cmd: str = "${{ PYTHON }} -m pip install ./${{ name|lower }}-${{ version }}.tar.gz -vv --no-deps --no-build-isolation"
     
     # modify file
-    #recipe["build"]["script"] = f"{curl_cmd} && {py_install_cmd}"
-    recipe["build"]["script"] = py_install_cmd
+    recipe["build"]["script"] = f"{curl_cmd} && {py_install_cmd}"
     
     # Write it
     with open(recipe_path, 'w') as recipe_file:
