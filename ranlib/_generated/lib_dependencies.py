3cdc41a7e55d9d65ad159a36dd8b8278aefb2faf
Ameer Arsala <ameer.arsala03@gmail.com>
2024-10-14 14:01:24 -0700

    result of hooks

--- ranlib/_generated/lib_dependencies.py
+++ ranlib/_generated/lib_dependencies.py
@@ -1,13 +1,4 @@
 ## GENERATED CODE ##
 ## DO NOT MODIFY DIRECTLY ##
 
-
-DEPENDENCIES_NAMES: list[str] = [
-    'typer',
-    'pydantic',
-    'tomli',
-    'tomli-w',
-    'gitpython',
-    'rich',
-    'httpx',
-]
+DEPENDENCIES_NAMES: list[str] = ['typer', 'pydantic', 'tomli', 'tomli-w', 'gitpython', 'rich', 'httpx']
